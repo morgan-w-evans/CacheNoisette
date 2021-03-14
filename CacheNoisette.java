7e9bd0946ff00865a6f9fbe4c191e2bf60e5d9fa
Evans <evansm9@vdi-sccubu-048.lancs.local>
2021-03-14 22:21:22 +0000

    Compiled in linux

--- CacheNoisette.java
+++ CacheNoisette.java
@@ -14,51 +14,6 @@ public class CacheNoisette
 {
     public static void main (String[] args)
     {
-        GUI u = new GUI();
-        GameBoard g = new GameBoard(u);
-        boolean play = true;
-        
-        Squirrel redUp = new Squirrel("red", 0, 0, 0);
-        Squirrel redRight = new Squirrel("red", 1, 0, 90);
-        Squirrel redDown = new Squirrel("red", 0, 1, 180);
-        Squirrel redLeft = new Squirrel("red", 0, 0, 270);
-
-        Squirrel greyUp = new Squirrel("grey", 0, 0, 0);
-        Squirrel greyRight = new Squirrel("grey", 1, 0, 90);
-        Squirrel greyDown = new Squirrel("grey", 0, 1, 180);
-        Squirrel greyLeft = new Squirrel("grey", 0, 0, 270);
-
-        Squirrel brownUp = new Squirrel("brown", 0, 0, 0);
-        Squirrel brownRight = new Squirrel("brown", 1, 0, 90);
-        Squirrel brownDown = new Squirrel("brown", 0, 1, 180);
-        Squirrel brownLeft = new Squirrel("brown", 0, 0, 270);
-
-        Squirrel blackUp = new Squirrel("black", 0, 0, 90);
-        Squirrel blackRight = new Squirrel("black", 1, 0, 90);
-        Squirrel blackDown = new Squirrel("black", 0, 1, 180);
-        Squirrel blackLeft = new Squirrel("black", 0, 0, 270);
-
-        while (play) {
-
-            System.out.print("");
-
-            if (u.level() == 1) {
-
-                g.add(redLeft);
-                g.move(redLeft, 1, 1);
-                g.add(greyUp);
-                
-                g.move(greyUp, 2, 2);
-                g.addFlowerBlock(2);
-                g.startTimer();
-            }
-            else if (u.level() == 101 | g.levelComplete()) {
-
-                g.remove(redLeft);
-                g.remove(greyUp);
-                g.removeFlowerBlock(2);
-            }
-
-        }
+        GUI i = new GUI();
     }
 }
